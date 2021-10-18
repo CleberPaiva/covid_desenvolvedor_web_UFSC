143bcc3892114f24fb876fd0fb0e1b7a48dbb108
Cleber Paiva <58832413+CleberPaiva@users.noreply.github.com>
2021-10-18 17:04:06 -0300

    segundo commit

---
+++ servidor.js
@@ -0,0 +1,16 @@
+var express = require("express")
+var handlebars = require("express-handlebars")
+var rotas = require("./rotas")
+
+var aplicacao = express()
+const PORTA = 8085
+
+aplicacao.engine("handlebars", handlebars({defaultLayout: "main"}))
+aplicacao.set("view engine", "handlebars")
+
+aplicacao.use(express.urlencoded({extended:true}))
+aplicacao.use(rotas)
+
+aplicacao.listen(PORTA,function(){
+				console.log('Servidor executando...')
+})
